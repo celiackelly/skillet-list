e17dc937c73db758c1114e830559f3497e41342b
Celia Kelly <celiackelly@gmail.com>
2022-07-28 13:50:00 -0400

    Refactor dish li button groups using OOP

--- public/js/main.js
+++ public/js/main.js
@@ -1,101 +1,99 @@
-//Select all element Nodes with the given class and assign to JS variables
-//can you refactor all these event listeners with event delegation?
-const deleteBtn = document.querySelectorAll('.fa-circle-xmark')
-const cookedBtn = document.querySelectorAll('.fa-utensils')
-const dish = document.querySelectorAll('.dish span')
-
-//Create an array from the NodeList 'deleteBtn'
-//Attach an event listener to each item in the array; on click, call deleteItem function 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteDish)
-})
-
-//Create an array from the NodeList 'cookedBtn'
-//Attach an event listener to each completed item; on click, call markNotCooked function
-Array.from(cookedBtn).forEach((element)=>{
-    element.addEventListener('click', markCooked)
-})
-
-//THIS NEEDS TO BE CHANGED
-//Create an array from the NodeList 'dish'
-//Attach an event listener to each item; on click, call markCooked function
-Array.from(dish).forEach((element)=>{
-    element.addEventListener('click', editDishInfo)
-})
-
-async function deleteDish(){
-    //Get the _id of the to-do item associated with this delete button
-    const dishID = this.parentNode.parentNode.children[0].children[1].id
-    try{
-        //Make a fetch call to the /deleteItem route on the server and send the todo item text in the body of the request  
-        const response = await fetch('deleteDish', {
-            //use the delete HTTP method
-            method: 'delete',
-            //declare the content-type of the data to be sent
-            headers: {'Content-Type': 'application/json'},
-            //make a JSON object to send in the body 
-            body: JSON.stringify({
-              'dishIDfromJS': dishID
-            })
-          })
-        //await the response from the server
-        const data = await response.json()
-        //log the server response
-        console.log(data)
-        //reload the page
-        location.reload()
-
-    //catch and log any errors
-    }catch(err){
-        console.log(err)
+class DishLiBtnGroup {
+    constructor(el) {
+        this._el = el
+        el.addEventListener('click', this.onClick.bind(this)) 
     }
-}
 
-async function markCooked(){
-    //Get the _id of the dish
-    const dishID = this.parentNode.parentNode.children[0].children[1].id
-    try{
-        //Make a fetch call to the /markCooked route on the server and send the dish _id in the body of the request  
-        const response = await fetch('markCooked', {
-            //use the PUT HTTP method
-            method: 'put',
-            //declare the content-type of the data to be sent
-            headers: {'Content-Type': 'application/json'},
-            //make a JSON object to send in the body 
-            body: JSON.stringify({
+    onClick(e) {
+        let action = e.target.dataset.action
+        if (action) {
+            this[action](e)
+        }
+    }
+
+    async markCooked(e) {
+        //Get the _id of the dish associated with this markCooked button
+        let li = e.target.closest('li')
+        if (!li) return
+        const dishID = li.id
+
+        try {
+            const response = await fetch('markCooked', {
+                method: 'put',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
                 'dishIDfromJS': dishID
+                })
             })
-          })
-        //await the response from the server
-        const data = await response.json()
-        //log the server response
-        console.log(data)
-        //reload the page
-        location.reload()
+            const data = await response.json()
+            console.log(data)
+            location.reload()
 
-    }catch(err){
-        //catch and log any errors
-        console.log(err)
+        } catch(err){
+            console.log(err)
+        }
     }
-}
 
-//Not updated yet - still marks as not cooked
-async function editDishInfo(){
-    const dishID = this.parentNode.children[0].id
-    try{
-        const response = await fetch('markNotCooked', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+    edit() {
+        alert('editing')   
+    }
+
+    async delete(e) {
+        //Get the _id of the dish associated with this delete button
+        let li = e.target.closest('li')
+        if (!li) return
+        const dishID = li.id
+
+        try {
+            const response = await fetch('deleteDish', {
+                method: 'delete',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
                 'dishIDfromJS': dishID
+                })
             })
-          })
+            const data = await response.json()
+            console.log(data)
+            location.reload()
 
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-
-    }catch(err){
-        console.log(err)
+        } catch(err){
+            console.log(err)
+        }
     }
 }
+
+const dishLiBtnGroups = document.querySelectorAll('li.dish .btn-group')
+
+Array.from(dishLiBtnGroups).forEach((btnGroup)=>{
+    new DishLiBtnGroup(btnGroup)
+})
+
+
+// async function markCooked(){
+//     //Get the _id of the dish
+//     const dishID = this.parentNode.parentNode.children[0].children[1].id
+//     try{
+//         //Make a fetch call to the /markCooked route on the server and send the dish _id in the body of the request  
+//         const response = await fetch('markCooked', {
+//             //use the PUT HTTP method
+//             method: 'put',
+//             //declare the content-type of the data to be sent
+//             headers: {'Content-Type': 'application/json'},
+//             //make a JSON object to send in the body 
+//             body: JSON.stringify({
+//                 'dishIDfromJS': dishID
+//             })
+//           })
+//         //await the response from the server
+//         const data = await response.json()
+//         //log the server response
+//         console.log(data)
+//         //reload the page
+//         location.reload()
+
+//     }catch(err){
+//         //catch and log any errors
+//         console.log(err)
+//     }
+// }
+
