b3ebcec09e4b3fd8a5b1e5e99e855d71ad72d0bb
Celia Kelly <celiackelly@gmail.com>
2022-07-28 23:17:46 -0400

    Implement express-ejs-layouts

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const { MongoClient, ObjectId } = require('mongodb')
 //Assign local port number
 const PORT = 2121
 
+const expressLayouts = require('express-ejs-layouts')
+
 //Configure .env file, so that you can import secrets 
 require('dotenv').config()
 
@@ -24,9 +26,12 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     })
 
 app.set('view engine', 'ejs')  //Set up app to use EJS as the HTML templating language
+app.set('layout', './layouts/layout')
+
 app.use(express.static('public'))  //Set up public folder to serve CSS, JS, and image files
 app.use(express.urlencoded({ extended: true })) // Use Express' built-in middleware to parse incoming requests with urlencoded payloads 
 app.use(express.json()) //Use Express' json parser middleware
+app.use(expressLayouts)
 
 //Set up the server to listen on our port (if it's defined in .env), or whatever the port is
 app.listen(process.env.PORT || PORT, ()=>{
