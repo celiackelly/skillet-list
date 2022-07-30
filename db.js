a999359f4d7117223aa9a56edb95627c27037c7d
Celia Kelly <celiackelly@gmail.com>
2022-07-30 00:15:52 -0400

    Fix Mongoose logging on db connection

--- db.js
+++ db.js
@@ -7,12 +7,11 @@ const connectDB = async () => {
             useNewUrlParser: true, 
             useUnifiedTopology: true,
         })
-        console.log(`MongoDB Connected: ${connection.host}`)
+        console.log(`MongoDB Connected: ${connection.connection.host}`)
     } catch(err) {
         console.error(err);
         process.exit(1);
     }
-
 }
 
 module.exports = connectDB
@@ -21,38 +20,3 @@ module.exports = connectDB
 /*
 Helpful article: https://mrvautin.com/re-use-mongodb-database-connection-in-routes/
 */
-
-//Import MongoDB library; destructure to get access to ObjectId 
-// const { MongoClient } = require('mongodb')
-
-// //Declare database, connection string, database name
-// let mongodb,  
-//     dbConnectionStr = process.env.DB_STRING,    //get db connection string from .env
-//     dbName = 'dishes'
-
-// async function connect() {
-//     try {
-//         const client = await MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-
-//         console.log(`Connected to ${dbName} database`) //once successfully connected, log message in console
-//         mongodb = client.db(dbName)  //assign the 'dishes' database to db variable
-
-//     } catch (err) {
-//         console.log(err)
-//         process.exit(1)
-//     }
-// }
-
-// function get(){
-//     return mongodb;
-// }
-
-// function close(){
-//     mongodb.close();
-// }
-
-// module.exports = {
-//     connect,
-//     get,
-//     close, 
-// };
